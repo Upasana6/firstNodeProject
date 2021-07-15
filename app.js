422ee72e25bee68d4e38a9d251857b4efdfe2866
Upasana <upasna.verma06@gmail.com>
2021-07-15 23:53:23 +0530

    added get function

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-const m = require('mongoose')
+const express = require('express')
+const app = express()
+
 console.log('Hello World')
+
+app.get('/', (req, res)=>{
+    console.log(req.url);
+    res.send('Home Page');
+});
+
+app.get('/movieList', (req, res)=>{
+    console.log('movie list')
+    res.status(200).send('Movies')
+})
+
+app.post('/addMovie', (req, res)=>{
+
+})
+
+app.get('/movieList/:id', (req, res)=>{
+    console.log(`id is ${req.params.id}`)
+    res.status(200).send(`Movie no ${req.params.id}`)
+})
+
+app.listen(8000)
